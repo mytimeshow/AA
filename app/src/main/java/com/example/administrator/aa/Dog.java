43f78fc6eb435fdd3ee05e0d8fbea206d4b8f433
zhanwen <zhanwen@gmail.com>
2017-02-21 20:44:47 +0800

    First commit.

---
+++ app/src/main/java/com/example/administrator/aa/Dog.java
@@ -0,0 +1,14 @@
+package com.example.administrator.aa;
+
+/**
+ * Created by Administrator on 2017/2/20 0020.
+ */
+public class Dog {
+    String name;
+    int dogId;
+    Dog(String name,int dogId){
+        this.dogId=dogId;
+        this.name=name;
+    }
+
+}
---
+++ app/src/main/java/com/example/administrator/aa/MyAdapter.java
@@ -0,0 +1,62 @@
+package com.example.administrator.aa;
+
+import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/2/20 0020.
+ */
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by yls on 2017/2/20.
+ */
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
+    private List<Dog> dogList;
+
+    public MyAdapter(List<Dog> dogList){
+        this.dogList = dogList;
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView dogImg;
+        TextView dogName;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            dogImg = (ImageView) itemView.findViewById(R.id.dogImg);
+            dogName = (TextView) itemView.findViewById(R.id.dogName);
+        }
+    }
+
+
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.dogitem, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Dog dog = dogList.get(position);
+        holder.dogImg.setImageResource(dog.dogId);
+        holder.dogName.setText(dog.name);
+    }
+
+
+    public int getItemCount() {
+        return dogList.size();
+    }
+
+
+}
